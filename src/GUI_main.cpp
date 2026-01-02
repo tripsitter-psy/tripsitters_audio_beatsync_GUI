8e477e33f713db01614fff87f9daf3c28fab8428
tripsitter-psy <tripsitter-psy@users.noreply.github.com>
2026-01-02 19:10:02 +1100

    Fix PNG image loading in GUI - add wxInitAllImageHandlers()

--- src/GUI_main.cpp
+++ src/GUI_main.cpp
@@ -1,15 +1,19 @@
 #include <wx/wx.h>
+#include <wx/image.h>
 #include "gui/MainWindow.h"
 
 class TripSitterApp : public wxApp {
 public:
     virtual bool OnInit() override {
+        // Initialize image handlers for PNG, JPEG, etc.
+        wxInitAllImageHandlers();
+
         SetAppName("TripSitter");
         SetVendorName("TripSitter");
-        
+
         MainWindow* frame = new MainWindow();
         frame->Show(true);
-        
+
         return true;
     }
 };
