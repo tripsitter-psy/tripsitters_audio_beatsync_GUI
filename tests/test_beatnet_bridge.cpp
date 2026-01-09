58ad0a5c653daf5284c71442fe3a8346bc62016b
tripsitter-psy <tripsitter-psy@users.noreply.github.com>
2026-01-10 05:52:40 +1100

    tests: add skeletons for FFTAnalyzer and BeatNetBridge and update CMakeLists

---
+++ tests/test_beatnet_bridge.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("BeatNetBridge skeleton - TODO implement", "[.beatnet][todo]") {
+    // TODO: Add tests to execute beatnet_analyze wrapper, parse JSON output, and handle errors
+    REQUIRE(true);
+}
